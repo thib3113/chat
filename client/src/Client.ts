6931238d45a86d7f1c4b964c79d7714b8d4313d7
thibaut severac <thib3113@gmail.com>
2021-10-13 16:18:45 +0200

    first ping pong between client and Server.ts

--- client/src/Client.ts
+++ client/src/Client.ts
@@ -1,9 +1,61 @@
+import {io, Socket} from "socket.io-client";
+
 export class Client {
+    private serverUrl: string;
+    private socket: Socket;
+
+
+    private readonly pingInterval = 5 * 1000; // 5s
+    private pingTimer?: number;
+    private pingStart?: Date;
+
+    /**
+     * init the client
+     */
     public async init(): Promise<this> {
+        this.serverUrl = process.env.CHAT_SERVER || 'http://localhost:3113'
+
         return this;
     }
 
     public async start(): Promise<this> {
+        this.socket = io(this.serverUrl);
+        //start the ping loop
+        this.pingLoop();
+
         return this;
+    }
+
+    public pingLoop() {
+        //if fn called but timer already started
+        if(this.pingTimer) {
+            clearTimeout(this.pingTimer);
+        }
+
+        //if socket is closed
+        if(!this.socket.active) {
+            clearTimeout(this.pingTimer);
+            return;
+        }
+
+        this.socket.once('pong', () => {
+            console.log('receive pong');
+            if(!this.pingStart) {
+                return;
+            }
+
+            const timing = Date.now() - this.pingStart.getTime();
+            console.log('emit ping_result : ', timing);
+
+            //send ping timing to the server, for stats
+            this.socket.emit('ping_result', timing);
+        });
+
+        console.log('emit ping');
+        this.pingStart = new Date();
+        this.socket.emit('ping');
+
+        //in some node version / configuration, setTimeout return a specific node object
+        this.pingTimer = setTimeout(() => this.pingLoop(), this.pingInterval) as unknown as number
     }
 }
--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,9 +1,58 @@
+import express, {Express} from "express";
+import http, { createServer } from "http";
+import {Server as SocketServer, Socket} from "socket.io";
+
 export class Server {
+    private httpServer: http.Server;
+    private app: Express;
+    private io: SocketServer;
+    private port;
+
     public async init(): Promise<this> {
+        this.app = express();
+        this.httpServer = createServer(this.app);
+        this.io = new SocketServer(this.httpServer, { /* options */ });
+
+        this.io.on('connection', (socket) => {
+            console.log('client connected : ', socket.id);
+
+            this.registerListeners(socket);
+        });
+
+        this.port = Number(process.env.PORT) || 3113;
+
         return this;
     }
 
     public async start(): Promise<this> {
+        await new Promise(resolve => {
+            this.httpServer.listen(this.port, () => {
+                console.log(`server listen on port ${this.port}`);
+                resolve(null);
+            });
+        })
         return this;
+    }
+
+    private registerListeners(socket: Socket) {
+        socket.use((pReq, next) => {
+            const req = [...pReq];
+            console.log(req.shift(), 'called with', ...req);
+            next();
+        });
+        //init pong response
+        socket.on('ping', () => {
+            socket.emit('pong');
+        });
+
+        socket.on('disconnect', () => {
+            console.log('client disconnected : ', socket.id);
+
+            this.unregisterListeners(socket);
+        });
+    }
+
+    private unregisterListeners(socket: Socket) {
+        socket.removeAllListeners();
     }
 }
